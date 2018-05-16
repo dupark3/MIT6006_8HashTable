2dcb02ac27b6e73d80770f686443028e8e137d23
DuParkLinux <dupark3@gmail.com>
2018-05-16 10:11:27 -0400

    Max defined and Hash[Max][2] table formed to allow negative numbers in Hash[][1]

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
+#define MAX 1000
+
+bool hash[MAX][2];
+
+void insert(int keys[], int size){
+
+}
+
 int main(){
     int keys[] = {1, -5, 2, 3, 0, -10, 15};
-    
+    int size = sizeof(keys) / sizeof(int);
+
 }
