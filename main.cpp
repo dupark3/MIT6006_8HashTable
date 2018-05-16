be603ec083875ff45ea9684210c1e346ce8bf7aa
DuParkLinux <dupark3@gmail.com>
2018-05-16 10:17:23 -0400

    Insert function implemented to allow both non-negative and negative ints. Takes the absolute value of negative key and stores it into the Hash 2D array at hash[abs(key)][1]

--- main.cpp
+++ main.cpp
@@ -1,15 +1,24 @@
+#include <cmath> // abs
 #include <iostream>
+
 
 #define MAX 1000
 
-bool hash[MAX][2];
+bool hash[MAX + 1][2];
 
 void insert(int keys[], int size){
-
+    for(int i = 0; i < size; ++i){
+        if (keys[i] >= 0){
+            hash[keys[i]][0] = true;
+        } else {
+            hash[std::abs(keys[i])][1] = true;
+        }
+    }
 }
 
 int main(){
     int keys[] = {1, -5, 2, 3, 0, -10, 15};
     int size = sizeof(keys) / sizeof(int);
+    insert(keys, size);
 
 }
